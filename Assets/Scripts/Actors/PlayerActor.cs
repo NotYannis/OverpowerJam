05d2e8272f1066cb8ba536dd0edb1288ec0d4e97
seamusoneill <seamusoneill@live.ie>
2018-11-14 19:57:13 +0100

    Twinstick player

--- Assets/Scripts/Actors/PlayerActor.cs
+++ Assets/Scripts/Actors/PlayerActor.cs
@@ -3,16 +3,19 @@ using StateControllerManagement;
 
 [RequireComponent (typeof (PlayerStateController))]
 [RequireComponent (typeof (SpriteRenderer))]
+[RequireComponent (typeof (PlayerController))]
 public class PlayerActor : MonoBehaviour
 {
     [HideInInspector]
     PlayerStateController playerStateController;
     [HideInInspector]
     SpriteRenderer spriteRenderer;
+    [HideInInspector] PlayerController playerController;
 
     private void Awake()
     {
         playerStateController = GetComponent<PlayerStateController>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        playerController = GetComponent<PlayerController>();
     }
 }
---
+++ Assets/Scripts/Controls/PlayerPrototypeMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using InControl;
+
+public class PlayerPrototypeMovement : MonoBehaviour
+{
+    Transform playerWeaponTransform;
+    private float speed  = 8;
+    private void Awake()
+    {
+        playerWeaponTransform = GetComponentInChildren<PlayerWeapon>().transform;
+    }
+
+    void Update()
+    {
+
+        transform.position += Vector3.right * InputManager.ActiveDevice.LeftStick.X * Time.deltaTime * speed;
+        transform.position += Vector3.up * InputManager.ActiveDevice.LeftStick.Y * Time.deltaTime * speed;
+
+        playerWeaponTransform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(InputManager.ActiveDevice.RightStick.Y, InputManager.ActiveDevice.RightStick.X) * 180 / Mathf.PI - 90);
+    }
+}
