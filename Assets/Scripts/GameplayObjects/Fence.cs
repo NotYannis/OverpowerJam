324f068f23228c708e18e5a976fd9081b2b03225
seamusoneill <seamusoneill@live.ie>
2018-11-16 11:46:12 +0100

    Fence and file cleanup

---
+++ Assets/Scripts/GameplayObjects/Fence.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Fence : MonoBehaviour
+{
+    LayerMask destructibles;
+
+    private void Awake()
+    {
+        destructibles = LayerMask.NameToLayer("Fruit");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == destructibles)
+        {
+            Debug.Log("Check 2");
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}
