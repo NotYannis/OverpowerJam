b348bdbaf75704f79b763abc2ee723618bec397a
seamusoneill <seamusoneill@live.ie>
2018-11-16 17:18:14 +0100

    Scoring system

--- Assets/Scripts/GameplayObjects/Fruit.cs
+++ Assets/Scripts/GameplayObjects/Fruit.cs
@@ -1,31 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
 
-	private LayerMask dirtLayer;
+    private LayerMask dirtLayer;
     private LayerMask bumperBushLayer;
     private LayerMask flowerBushLayer;
 
     [HideInInspector] public new Rigidbody2D rigidbody;
-	[SerializeField] private GameObject shadow;
-	[SerializeField] float height;
+    [SerializeField] private GameObject shadow;
+    [SerializeField] float height;
     public SpriteRenderer spriteRenderer;
 
-	public const float offset = -0.4f;
-	private Vector2 acceleration;
-	private float gravityVelocity;
+    public const float offset = -0.4f;
+    private Vector2 acceleration;
+    private float gravityVelocity;
 
     [SerializeField]
     private float bumperForce = 3;
 
-	private bool falling;
+    private bool falling;
+
+    int multiplier = 0;
 
     void Awake()
     {
-	    rigidbody = GetComponentInChildren<Rigidbody2D>();
-	    lastPosition = rigidbody.position.y;
-	    shadow = Instantiate(shadow, transform.position + Vector3.down * height, Quaternion.identity);
+        rigidbody = GetComponentInChildren<Rigidbody2D>();
+        lastPosition = rigidbody.position.y;
+        shadow = Instantiate(shadow, transform.position + Vector3.down * height, Quaternion.identity);
         spriteRenderer = GetComponent<SpriteRenderer>();
 
         dirtLayer = LayerMask.NameToLayer("Dirt");
@@ -34,80 +38,91 @@ public class Fruit : MonoBehaviour
     }
 
     private void Start()
-	{
-		if (GameStateController.Instance.gameConfig != null)
-		{
-			rigidbody.mass = GameStateController.Instance.gameConfig.fruitMass;
-			rigidbody.drag = GameStateController.Instance.gameConfig.fruitLinearDrag;
-			rigidbody.angularDrag = GameStateController.Instance.gameConfig.fruitAngularDrag;
-			rigidbody.gravityScale = GameStateController.Instance.gameConfig.fruitGravityScale;
-		}
-	}
+    {
+        if (GameStateController.Instance.gameConfig != null)
+        {
+            rigidbody.mass = GameStateController.Instance.gameConfig.fruitMass;
+            rigidbody.drag = GameStateController.Instance.gameConfig.fruitLinearDrag;
+            rigidbody.angularDrag = GameStateController.Instance.gameConfig.fruitAngularDrag;
+            rigidbody.gravityScale = GameStateController.Instance.gameConfig.fruitGravityScale;
+        }
+    }
 
-	private void OnEnable()
-	{
-		shadow.SetActive(true);
-	}
+    private void OnEnable()
+    {
+        shadow.SetActive(true);
+    }
 
-	private void OnDisable()
-	{
-		if(shadow != null)
-			shadow.SetActive(false);
-	}
+    private void OnDisable()
+    {
+        if (shadow != null)
+            shadow.SetActive(false);
+    }
 
-	public void Fall()
-	{
-		falling = true;
-		rigidbody.bodyType = RigidbodyType2D.Dynamic;
-	}
+    public void Fall()
+    {
+        falling = true;
+        rigidbody.bodyType = RigidbodyType2D.Dynamic;
+    }
 
-	private float lastPosition;
-	private void Update ()
-	{
-		shadow.transform.localScale = transform.lossyScale;
-	}
+    private float lastPosition;
+    private void Update()
+    {
+        shadow.transform.localScale = transform.lossyScale;
 
-	private void FixedUpdate()
-	{
-		Vector3 pos = rigidbody.position;
-		pos.y -= height + offset;
-		shadow.transform.position = pos;
+        if (rigidbody.velocity.SqrMagnitude() < 0.12f)
+        { multiplier = 0;
 
-		if (falling)
-		{
-			height += rigidbody.position.y - lastPosition;
-		}
-		lastPosition = rigidbody.position.y;
-	}
+        }
+    }
 
-	private void OnCollisionEnter2D(Collision2D other)
-	{
-		if (other.gameObject == shadow)
-		{
-			falling = false;
-			other.gameObject.GetComponent<Collider2D>().enabled = false;
-			rigidbody.gravityScale = 0f;
-			height = 0.5f;
-		}
+    private void FixedUpdate()
+    {
+        Vector3 pos = rigidbody.position;
+        pos.y -= height + offset;
+        shadow.transform.position = pos;
 
-		if (other.gameObject.layer == dirtLayer)
-		{
-			Destroy(gameObject);
-			other.gameObject.GetComponent<Collider2D>().enabled = false;
-			Instantiate(GameStateController.Instance.gameConfig.tree, other.transform.position, Quaternion.identity, other.transform);
-		}
+        if (falling)
+        {
+            height += rigidbody.position.y - lastPosition;
+        }
+        lastPosition = rigidbody.position.y;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject == shadow)
+        {
+            falling = false;
+            other.gameObject.GetComponent<Collider2D>().enabled = false;
+            rigidbody.gravityScale = 0f;
+            height = 0.5f;
+        }
+
+        if (other.gameObject.layer == dirtLayer)
+        {
+            Score.Instance.IncreaseScore(1000);
+            Destroy(gameObject);
+            other.gameObject.GetComponent<Collider2D>().enabled = false;
+
+            Instantiate(GameStateController.Instance.gameConfig.tree, other.transform.position, Quaternion.identity, other.transform);
+        }
 
         if (other.gameObject.layer == bumperBushLayer)
         {
+            multiplier++;
+            Score.Instance.IncreaseScore(100 * multiplier);
             Vector2 direction = gameObject.transform.position - other.gameObject.transform.position;
             direction = direction.normalized;
-            other.gameObject.GetComponent<Animator>().SetBool("Bumped",true);
+            other.gameObject.GetComponent<Animator>().SetBool("Bumped", true);
+
 
             rigidbody.AddForce(direction * bumperForce * Time.deltaTime, ForceMode2D.Impulse);
         }
 
         if (other.gameObject.layer == flowerBushLayer)
         {
+            Score.Instance.IncreaseScore(50);
             Vector2 direction = gameObject.transform.position - other.gameObject.transform.position;
             direction = direction.normalized;
 
@@ -125,4 +140,33 @@ public class Fruit : MonoBehaviour
             //rigidbody.AddForce(rigidbody.velocity.magnitude * 2 * direction.normalized);
         }
     }
+
+    bool perSecond = true;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (perSecond)
+        {
+            if (other.layer == LayerMask.NameToLayer("SoftWater"))
+            {
+                Score.Instance.IncreaseScore(1);
+                perSecond = false;
+                StartCoroutine("PerSecond");
+            }
+            if (other.layer == LayerMask.NameToLayer("StrongWater"))
+            {
+                Score.Instance.IncreaseScore(5);
+                perSecond = false;
+                StartCoroutine("PerSecond");
+            }
+        }
+    }
+
+    private IEnumerator PerSecond()
+    {
+        yield return new WaitForSeconds(1);
+        perSecond = true;
+        yield return null;
+    }
+    
 }
