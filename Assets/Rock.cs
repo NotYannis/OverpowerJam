5bf4f06f34684a4b3ac649f03b77f324a4123cad
seamusoneill <seamusoneill@live.ie>
2018-11-16 11:27:58 +0100

    Rock

---
+++ Assets/Rock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    LayerMask destroyers;
+
+    [SerializeField]
+    float shrinkRate = 5;
+
+    private void Awake()
+    {
+        destroyers = LayerMask.NameToLayer("StrongWater");
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.layer == destroyers)
+        {
+            transform.localScale -= new Vector3(1, 1, 0) * shrinkRate * Time.deltaTime;
+
+            if (transform.localScale.x < 0.01f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
